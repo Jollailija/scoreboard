5e26f69edf9f2fa14241b4739aa5e4c692063d02
jollailija <jollailija@gmail.com>
2015-09-25 16:18:09 +0300

    Fixed Swedish translation
    Ready for release 0.1.1

--- translations/harbour-scoreboard-sv.ts
+++ translations/harbour-scoreboard-sv.ts
@@ -9,7 +9,7 @@
     </message>
     <message>
         <source>A simple scoreboard app for Sailfish OS. Made by jollailija. Icon by Moth.</source>
-        <translation>En enkel resultattavel-app för SailfishOS. Skapad av jollailija. Ikon av Moth.</translation>
+        <translation>En enkel resultattavel-app för SailfishOS. Skapad av jollailija. Ikon av Moth. Svensk översättning av eson57.</translation>
     </message>
 </context>
 <context>
@@ -28,27 +28,27 @@
     </message>
     <message>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Allmänt</translation>
     </message>
     <message>
         <source>PullDownMenu</source>
-        <translation type="unfinished"></translation>
+        <translation>Rullgardinsmeny</translation>
     </message>
     <message>
         <source>* To reset the scores or the won rounds, use the &apos;Reset scores/rounds&apos;-action in the PullDownMenu. This will reset the highlighted value. You have three seconds to cancel it by tapping the remorse notification.</source>
-        <translation type="unfinished"></translation>
+        <translation>* För att återställa poäng eller vunna rundor, använder du &apos;Återställ poängställning/rundor&apos; i rullgardinsmenyn. Detta kommer att nollställa det markerade värdet. Du har tre sekunder på dig att ångra återställningen genom att trycka på återställningsmeddelandet.</translation>
     </message>
     <message>
         <source>* Use &apos;Saved scores&apos; in the PullDownMenu to fetch previous scores and rounds from the database. Beware that the saved values will be overwritten after any following changes.</source>
-        <translation type="unfinished"></translation>
+        <translation>* Använd &apos;Sparade poängställningar&apos; för att hämta tidigare poängställningar och rundor från databasen. Notera att sparade värden kommer att skrivas över om du ändrar något.</translation>
     </message>
     <message>
         <source>* To set new values directly, use the &apos;Set scores &amp;amp; rounds&apos; menu item and fill them in the corresponding text fields.</source>
-        <translation type="unfinished"></translation>
+        <translation>* För att ange nya värden direkt, använder du &apos;Ange poäng &amp; rundor&apos; i menyn, där du fyller i respektive textfält.</translation>
     </message>
     <message>
         <source>* Tap the “+” and “-“ buttons to change team scores. You can also add points using the cover actions. To change numbers of won rounds, tap first the small round numbers followed by tapping the +/- buttons. Get back to changing team scores by tapping the score numbers. Values are always automatically saved when modified.</source>
-        <translation type="unfinished"></translation>
+        <translation>* Tryck på plus- och minusknapparna (+/-) för att ändra lagens poäng. Du kan också lägga till poäng via programminiatyren. För att ändra antalet vunna rundor, trycker du först de små runda siffrorna följt av +/-. Återgå till lagpoängen genom att trycka på poängsiffran. Värdena sparas alltid automatiskt vid varje ändring.</translation>
     </message>
 </context>
 <context>
