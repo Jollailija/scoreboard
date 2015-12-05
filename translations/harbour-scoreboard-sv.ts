878c9b278a75675c37b8b6198affb9f523eeb654
Åke Engelbrektson <eson57@users.noreply.github.com>
2015-12-05 21:46:19 +0100

    Update harbour-scoreboard-sv.ts

--- translations/harbour-scoreboard-sv.ts
+++ translations/harbour-scoreboard-sv.ts
@@ -8,8 +8,8 @@
         <translation>Om</translation>
     </message>
     <message>
-        <source>A simple scoreboard app for Sailfish OS. Made by jollailija. Icon by Moth.</source>
-        <translation>En enkel resultattavel-app för SailfishOS. Skapad av jollailija. Ikon av Moth. Svensk översättning av eson57.</translation>
+        <source>A simple scoreboard app for Sailfish OS. Made by jollailija. Icon by Moth. Thanks to kimmoli for helping me out with the noarch packaging.</source>
+        <translation>En enkel resultattavla för SailfishOS. Skapad av jollailija. Ikon av Moth. Tack till kimmoli, för hjälp med noarch-paketeringen.</translation>
     </message>
 </context>
 <context>
@@ -28,104 +28,104 @@
     </message>
     <message>
         <source>* To set new values directly, use the &apos;Set scores &amp;amp; rounds&apos; menu item and fill them in the corresponding text fields.</source>
-        <translation>* För att ange nya värden direkt, använder du &apos;Ange poäng &amp; rundor&apos; i menyn, där du fyller i respektive textfält.</translation>
+        <translation>* För att ange nya värden direkt, går du till &apos;Ange poäng &amp;amp; omgångar&apos; i menyn och fyller i respektive textfält.</translation>
     </message>
     <message>
         <source>* Tap the “+” and “-“ buttons to change team scores. You can also add points using the cover actions. To change numbers of won rounds, tap first the small round numbers followed by tapping the +/- buttons. Get back to changing team scores by tapping the score numbers. Values are always automatically saved when modified.</source>
-        <translation>* Tryck på plus- och minusknapparna (+/-) för att ändra lagens poäng. Du kan också lägga till poäng via programminiatyren. För att ändra antalet vunna rundor, trycker du först de små runda siffrorna följt av +/-. Återgå till lagpoängen genom att trycka på poängsiffran. Värdena sparas alltid automatiskt vid varje ändring.</translation>
+        <translation>* Tryck på plus- och minusknapparna (+/-) för att ändra lagens poäng. Du kan också lägga till poäng via programminiatyren. För att ändra antalet vunna omgångar, trycker du först de små runda siffrorna följt av +/-. Återgå till lagpoängen genom att trycka på poängsiffran. Värdena sparas alltid automatiskt vid varje ändring.</translation>
     </message>
     <message>
         <source>PushUpMenu</source>
-        <translation type="unfinished"></translation>
+        <translation>Dra upp-meny</translation>
     </message>
     <message>
         <source>Multiple player view</source>
-        <translation type="unfinished"></translation>
+        <translation>Multispelarvy</translation>
     </message>
     <message>
         <source>* To reset the scores or the won rounds, use the &apos;Reset scores/rounds&apos;-action in the PushUpMenu. This will reset the highlighted value. You have three seconds to cancel it by tapping the remorse notification.</source>
-        <translation type="unfinished"></translation>
+        <translation>* För att återställa poängställning eller vunna omgångar, går du till &apos;Återställ poäng/omgångar&apos; i Dra upp-menyn. Detta återställer det markerade värdet. Du har tre sekunder på dig att avbryta, genom att trycka på ångermeddelandet.</translation>
     </message>
     <message>
         <source>* Use &apos;Saved scores&apos; in the PushUpMenu to fetch previous scores and rounds from the database. Beware that the saved values will be overwritten after any following changes.</source>
-        <translation type="unfinished"></translation>
+        <translation>* Använd dra upp-menyns &apos;Sparade poängställningar&apos; för att hämta tidigare poäng och omgångar från databasen. Notera att sparade värden kommer att skrivas över, av alla påföljande ändringar.</translation>
     </message>
     <message>
         <source>* You can swap the scores by selecting &apos;Swap&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>* Du kan växla poängställningar genom att välja &apos;Växla&apos;.</translation>
     </message>
     <message>
         <source>* Click on a players name to remove them from the list. There is a 5 second remorse before deletation.</source>
-        <translation type="unfinished"></translation>
+        <translation>* Tryck på en spelares namn för att ta bort den från listan. Det är 5 sekunders ångertid före borttagningen.</translation>
     </message>
     <message>
         <source>* You can remove all players by using the PushUpMenu at the bottom of the list. There is also a 5 second remorse before deletation.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>* Please note that scores at the Multiple Player View won&apos;t be kept on exit (yet).</source>
-        <translation type="unfinished"></translation>
+        <translation>* Du kan ta bort alla spelare med hjälp av dra upp-menyn i slutet på listan. Där finns också 5 sekunders ångertid före borttagning.</translation>
     </message>
     <message>
         <source>Two teams view</source>
-        <translation type="unfinished"></translation>
+        <translation>Tvålagsvy</translation>
     </message>
     <message>
         <source>* To add a player, input the player&apos;s name into the textfield and press enter.</source>
-        <translation type="unfinished"></translation>
+        <translation>* Mata in spelarens namn i textfältet och tryck retur, för att lägga till en spelare.</translation>
+    </message>
+    <message>
+        <source>Please note that scores at the Multiple Player View won&apos;t be kept on exit (yet).</source>
+        <translation>Notera att poängställningar i multispelarvyn inte bevaras vid avslut (ännu).</translation>
     </message>
 </context>
 <context>
     <name>MultiplePlayers</name>
     <message>
         <source>Scoreboard</source>
-        <translation type="unfinished">Resultattavla</translation>
+        <translation>Resultattavla</translation>
     </message>
     <message>
         <source>Add a new player</source>
-        <translation type="unfinished"></translation>
+        <translation>Lägg till ny spelare</translation>
     </message>
     <message>
         <source>Player name</source>
-        <translation type="unfinished"></translation>
+        <translation>Spelarnamn</translation>
     </message>
     <message>
         <source>Removing player</source>
-        <translation type="unfinished"></translation>
+        <translation>Tar bort spelaren</translation>
     </message>
     <message>
         <source>Remove all players</source>
-        <translation type="unfinished"></translation>
+        <translation>Ta bort alla spelare</translation>
     </message>
     <message>
         <source>Removing players</source>
-        <translation type="unfinished"></translation>
+        <translation>Tar bort spelare</translation>
     </message>
 </context>
 <context>
     <name>PulleyMenu</name>
     <message>
         <source>Multiple player view</source>
-        <translation type="unfinished"></translation>
+        <translation>Multispelarvy</translation>
     </message>
     <message>
         <source>Two teams view</source>
-        <translation type="unfinished"></translation>
+        <translation>Tvålagsvy</translation>
     </message>
     <message>
         <source>About</source>
-        <translation type="unfinished">Om</translation>
+        <translation>Om</translation>
     </message>
     <message>
         <source>Help</source>
-        <translation type="unfinished">Hjälp</translation>
+        <translation>Hjälp</translation>
     </message>
 </context>
 <context>
     <name>TTSetScores</name>
     <message>
         <source>Set scores</source>
-        <translation>Ange poängställning</translation>
+        <translation>Ange poängställningar</translation>
     </message>
     <message>
         <source>Home score</source>
@@ -137,7 +137,7 @@
     </message>
     <message>
         <source>Home score set.</source>
-        <translation>Hemmapoäng klar.</translation>
+        <translation>Hemmapoäng angivet.</translation>
     </message>
     <message>
         <source>Visitor score</source>
@@ -149,31 +149,31 @@
     </message>
     <message>
         <source>Visitor score set.</source>
-        <translation>Bortapoäng klar.</translation>
+        <translation>Bortapoäng angivet</translation>
     </message>
     <message>
         <source>Home rounds</source>
-        <translation>Hemmarundor</translation>
+        <translation>Hemmaomgångar</translation>
     </message>
     <message>
         <source>Set home rounds</source>
-        <translation>Ange hemmarundor</translation>
+        <translation>Ange hemmaomgångar</translation>
     </message>
     <message>
         <source>Home rounds set.</source>
-        <translation>Hemmarundor klart.</translation>
+        <translation>Hemmaomgångar angivet.</translation>
     </message>
     <message>
         <source>Visitor rounds</source>
-        <translation>Bortarundor</translation>
+        <translation>Bortaomgångar</translation>
     </message>
     <message>
         <source>Set visitor rounds</source>
-        <translation>Ange bortarundor</translation>
+        <translation>Ange bortaomgångar</translation>
     </message>
     <message>
         <source>Visitor rounds set.</source>
-        <translation>Bortarundor Klart.</translation>
+        <translation>Bortaomgångar angivet</translation>
     </message>
     <message>
         <source>Press enter to save</source>
@@ -188,7 +188,7 @@
     </message>
     <message>
         <source>Resetting rounds</source>
-        <translation>Återställer rundor</translation>
+        <translation>Återställer omgångar</translation>
     </message>
     <message>
         <source>Saved scores</source>
@@ -196,7 +196,7 @@
     </message>
     <message>
         <source>Set scores &amp; rounds</source>
-        <translation>Ange poäng &amp; rundor</translation>
+        <translation>Ange poäng &amp; omgångar</translation>
     </message>
     <message>
         <source>Reset scores</source>
@@ -204,7 +204,7 @@
     </message>
     <message>
         <source>Reset rounds</source>
-        <translation>Återställ rundor</translation>
+        <translation>Återställ omgångar</translation>
     </message>
     <message>
         <source>Scoreboard</source>
@@ -219,11 +219,12 @@
     <name>harbour-scoreboard</name>
     <message>
         <source>Open the app</source>
-        <translation type="unfinished"></translation>
+        <translation>Öppna appen</translation>
     </message>
     <message>
         <source>Warning!</source>
-        <translation type="unfinished"></translation>
+        <translation>Varning!</translation>
     </message>
 </context>
 </TS>
+
