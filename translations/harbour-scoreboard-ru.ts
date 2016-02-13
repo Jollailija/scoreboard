fff1c7ed8ca4d54a45a0e7f42dc0dcbc67c6e87d
InnaLisovskaya <snowisland@list.ru>
2016-02-13 15:09:35 +0500

    Create harbour-scoreboard-ru.ts

---
+++ translations/harbour-scoreboard-ru.ts
@@ -0,0 +1,218 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A simple scoreboard app for Sailfish OS. Made by jollailija. Icon by Moth. Thanks to kimmoli for helping me out with the noarch packaging.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Help</name>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>How to use</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>* To set new values directly, use the &apos;Set scores &amp;amp; rounds&apos; menu item and fill them in the corresponding text fields.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>* Tap the “+” and “-“ buttons to change team scores. You can also add points using the cover actions. To change numbers of won rounds, tap first the small round numbers followed by tapping the +/- buttons. Get back to changing team scores by tapping the score numbers. Values are always automatically saved when modified.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>PushUpMenu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Multiple player view</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>* To reset the scores or the won rounds, use the &apos;Reset scores/rounds&apos;-action in the PushUpMenu. This will reset the highlighted value. You have three seconds to cancel it by tapping the remorse notification.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>* Use &apos;Saved scores&apos; in the PushUpMenu to fetch previous scores and rounds from the database. Beware that the saved values will be overwritten after any following changes.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>* You can swap the scores by selecting &apos;Swap&apos;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>* Click on a players name to remove them from the list. There is a 5 second remorse before deletation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>* You can remove all players by using the PushUpMenu at the bottom of the list. There is also a 5 second remorse before deletation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Two teams view</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>* To add a player, input the player&apos;s name into the textfield and press enter.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please note that scores at the Multiple Player View won&apos;t be kept on exit (yet).</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MultiplePlayers</name>
+    <message>
+        <source>Scoreboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add a new player</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Player name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Removing player</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove all players</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Removing players</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PulleyMenu</name>
+    <message>
+        <source>Multiple player view</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Two teams view</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TTSetScores</name>
+    <message>
+        <source>Set scores</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Home score</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set home score</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Home score set.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Visitor score</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set visitor score</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Visitor score set.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Home rounds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set home rounds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Home rounds set.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Visitor rounds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set visitor rounds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Visitor rounds set.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Press enter to save</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TTView</name>
+    <message>
+        <source>Resetting scores</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Resetting rounds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Saved scores</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set scores &amp; rounds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reset scores</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reset rounds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Scoreboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Swap</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
