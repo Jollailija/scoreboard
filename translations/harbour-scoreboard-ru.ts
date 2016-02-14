bc6e4aea7aaacd346fcc7ffaf4eaa1661751239d
InnaLisovskaya <snowisland@list.ru>
2016-02-14 18:42:02 +0500

    Update harbour-scoreboard-ru.ts

--- translations/harbour-scoreboard-ru.ts
+++ translations/harbour-scoreboard-ru.ts
@@ -16,11 +16,11 @@
     <name>Help</name>
     <message>
         <source>About</source>
-        <translation type="unfinished">О программе</translation>
+        <translation>О программе</translation>
     </message>
     <message>
         <source>How to use</source>
-        <translation type="unfinished">Как пользоваться</translation>
+        <translation>Как пользоваться</translation>
     </message>
     <message>
         <source></source>
@@ -28,7 +28,7 @@
     </message>
     <message>
         <source>* To set new values directly, use the &apos;Set scores &amp;amp; rounds&apos; menu item and fill them in the corresponding text fields.</source>
-        <translation type="unfinished">Чтобы установить новые значения напрямую, выберите пункт меню "Установка счета" и заполните соответствующие текстовые поля.</translation>
+        <translation>Чтобы установить новые значения напрямую, выберите пункт меню "Установка счета" и заполните соответствующие текстовые поля.</translation>
     </message>
     <message>
         <source>* Tap the “+” and “-“ buttons to change team scores. You can also add points using the cover actions. To change numbers of won rounds, tap first the small round numbers followed by tapping the +/- buttons. Get back to changing team scores by tapping the score numbers. Values are always automatically saved when modified.</source>
@@ -36,120 +36,120 @@
     </message>
     <message>
         <source>PushUpMenu</source>
-        <translation type="unfinished"></translation>
+        <translation>Всплывающее меню</translation>
     </message>
     <message>
         <source>Multiple player view</source>
-        <translation type="unfinished"></translation>
+        <translation>Если игроков больше двух</translation>
     </message>
     <message>
         <source>* To reset the scores or the won rounds, use the &apos;Reset scores/rounds&apos;-action in the PushUpMenu. This will reset the highlighted value. You have three seconds to cancel it by tapping the remorse notification.</source>
-        <translation type="unfinished"></translation>
+        <translation>Чтобы скинуть счет или законченные раунды, используйте пункт меню "Сбросить счет". Это обнулит подсвеченные значения. У вас есть 3 секунды, чтобы отменить обнуление, нажав на предупреждение вверху.</translation>
     </message>
     <message>
         <source>* Use &apos;Saved scores&apos; in the PushUpMenu to fetch previous scores and rounds from the database. Beware that the saved values will be overwritten after any following changes.</source>
-        <translation type="unfinished"></translation>
+        <translation>Чтобы увидеть счет предыдущих раундов используйте пункт меню "Сохраненный счет". Учтите, что значения будут перезаписаны после любых изменений.</translation>
     </message>
     <message>
         <source>* You can swap the scores by selecting &apos;Swap&apos;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Вы можете поменять очки местами, нажав "Поменять местами".</translation>
     </message>
     <message>
         <source>* Click on a players name to remove them from the list. There is a 5 second remorse before deletation.</source>
-        <translation type="unfinished"></translation>
+        <translation>Нажмите на имя игрока, чтобы удалить его из списка. У вас есть 5 секунд, чтобы передумать.</translation>
     </message>
     <message>
         <source>* You can remove all players by using the PushUpMenu at the bottom of the list. There is also a 5 second remorse before deletation.</source>
-        <translation type="unfinished"></translation>
+        <translation>Вы можете удалить сразу всех игроков используя самый нижний пункт меню. У вас также есть 5 секунд, чтобы отменить решение.</translation>
     </message>
     <message>
         <source>Two teams view</source>
-        <translation type="unfinished"></translation>
+        <translation>Режим двух команд</translation>
     </message>
     <message>
         <source>* To add a player, input the player&apos;s name into the textfield and press enter.</source>
-        <translation type="unfinished"></translation>
+        <translation>Чтобы добавить нового игрока, введите его имя в текстовое поле и нажмите ввод.</translation>
     </message>
     <message>
         <source>Please note that scores at the Multiple Player View won&apos;t be kept on exit (yet).</source>
-        <translation type="unfinished"></translation>
+        <translation>Учтите, что очки в режиме игры более двух команд не будут сохранены при выходе (пока).</translation>
     </message>
 </context>
 <context>
     <name>MultiplePlayers</name>
     <message>
         <source>Scoreboard</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Игровое табло</translation>
     </message>
     <message>
         <source>Add a new player</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Добавить нового игрока</translation>
     </message>
     <message>
         <source>Player name</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Имя игрока</translation>
     </message>
     <message>
         <source>Removing player</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Удаление игрока</translation>
     </message>
     <message>
         <source>Remove all players</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Удалить всех</translation>
     </message>
     <message>
         <source>Removing players</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Удаление всех игроков</translation>
     </message>
 </context>
 <context>
     <name>PulleyMenu</name>
     <message>
         <source>Multiple player view</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Более двух команд</translation>
     </message>
     <message>
         <source>Two teams view</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Режим двух команд</translation>
     </message>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">О программе</translation>
     </message>
     <message>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Помощь</translation>
     </message>
 </context>
 <context>
     <name>TTSetScores</name>
     <message>
         <source>Set scores</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Установить счет</translation>
     </message>
     <message>
         <source>Home score</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Счет хозяев</translation>
     </message>
     <message>
         <source>Set home score</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Установить счет хозяев</translation>
     </message>
     <message>
         <source>Home score set.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Установка счета хозяев</translation>
     </message>
     <message>
         <source>Visitor score</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Счет гостей</translation>
     </message>
     <message>
         <source>Set visitor score</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Установить счет гостей</translation>
     </message>
     <message>
         <source>Visitor score set.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Установка счета гостей</translation>
     </message>
     <message>
         <source>Home rounds</source>
