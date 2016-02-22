83da50857c4a74d602a939daacae38bb0a428705
jollailija <Jollailija@users.noreply.github.com>
2016-02-22 17:55:15 +0200

    Russian translation by InnaLisovskaya
    See https://github.com/InnaLisovskaya/scoreboard/pull/1

---
+++ translations/harbour-scoreboard-ru.ts
@@ -0,0 +1,218 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>A simple scoreboard app for Sailfish OS. Made by jollailija. Icon by Moth. Thanks to kimmoli for helping me out with the noarch packaging.</source>
+        <translation>Простое приложение для ведения игрового счета под Sailfish OS. Сделано jollailija. Иконка от Moth. Спасибо kimmoli за помощь с noarch.</translation>
+    </message>
+</context>
+<context>
+    <name>Help</name>
+    <message>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>How to use</source>
+        <translation>Как пользоваться</translation>
+    </message>
+    <message>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>* To set new values directly, use the &apos;Set scores &amp;amp; rounds&apos; menu item and fill them in the corresponding text fields.</source>
+        <translation>Чтобы установить новые значения напрямую, выберите пункт меню "Установка счета" и заполните соответствующие текстовые поля.</translation>
+    </message>
+    <message>
+        <source>* Tap the “+” and “-“ buttons to change team scores. You can also add points using the cover actions. To change numbers of won rounds, tap first the small round numbers followed by tapping the +/- buttons. Get back to changing team scores by tapping the score numbers. Values are always automatically saved when modified.</source>
+        <translation>Нажимайте кнопки “+” и “-“, чтобы изменить счет. Вы также можете добавить очки, используя тачпад. Чтобы изменить количество выигранных раундов нажмите сначала на цифры, означающие их количество, и измените его нажимая на + и -. Измените очки команд, нажав на цифры, обозначающие очки. Все изменения сохраняются автоматически.</translation>
+    </message>
+    <message>
+        <source>PushUpMenu</source>
+        <translation>Всплывающее меню</translation>
+    </message>
+    <message>
+        <source>Multiple player view</source>
+        <translation>Более двух игроков</translation>
+    </message>
+    <message>
+        <source>* To reset the scores or the won rounds, use the &apos;Reset scores/rounds&apos;-action in the PushUpMenu. This will reset the highlighted value. You have three seconds to cancel it by tapping the remorse notification.</source>
+        <translation>Чтобы скинуть счет или законченные раунды, используйте пункт меню "Сбросить счет". Это обнулит подсвеченные значения. У вас есть 3 секунды, чтобы отменить обнуление, нажав на предупреждение вверху.</translation>
+    </message>
+    <message>
+        <source>* Use &apos;Saved scores&apos; in the PushUpMenu to fetch previous scores and rounds from the database. Beware that the saved values will be overwritten after any following changes.</source>
+        <translation>Чтобы увидеть счет предыдущих раундов используйте пункт меню "Сохраненный счет". Учтите, что значения будут перезаписаны после любых изменений.</translation>
+    </message>
+    <message>
+        <source>* You can swap the scores by selecting &apos;Swap&apos;.</source>
+        <translation>Вы можете поменять очки местами, нажав "Поменять местами".</translation>
+    </message>
+    <message>
+        <source>* Click on a players name to remove them from the list. There is a 5 second remorse before deletation.</source>
+        <translation>Нажмите на имя игрока, чтобы удалить его из списка. У вас есть 5 секунд, чтобы передумать.</translation>
+    </message>
+    <message>
+        <source>* You can remove all players by using the PushUpMenu at the bottom of the list. There is also a 5 second remorse before deletation.</source>
+        <translation>Вы можете удалить сразу всех игроков, используя самый нижний пункт меню. У вас также есть 5 секунд, чтобы отменить решение.</translation>
+    </message>
+    <message>
+        <source>Two teams view</source>
+        <translation>Режим двух команд</translation>
+    </message>
+    <message>
+        <source>* To add a player, input the player&apos;s name into the textfield and press enter.</source>
+        <translation>Чтобы добавить нового игрока, введите его имя в текстовое поле и нажмите ввод.</translation>
+    </message>
+    <message>
+        <source>Please note that scores at the Multiple Player View won&apos;t be kept on exit (yet).</source>
+        <translation>Учтите, что очки в режиме игры более двух команд не будут сохранены при выходе (пока).</translation>
+    </message>
+</context>
+<context>
+    <name>MultiplePlayers</name>
+    <message>
+        <source>Scoreboard</source>
+        <translation>Игровое табло</translation>
+    </message>
+    <message>
+        <source>Add a new player</source>
+        <translation>Добавить нового игрока</translation>
+    </message>
+    <message>
+        <source>Player name</source>
+        <translation>Имя игрока</translation>
+    </message>
+    <message>
+        <source>Removing player</source>
+        <translation>Удаление игрока</translation>
+    </message>
+    <message>
+        <source>Remove all players</source>
+        <translation>Удалить всех</translation>
+    </message>
+    <message>
+        <source>Removing players</source>
+        <translation>Удаление всех игроков</translation>
+    </message>
+</context>
+<context>
+    <name>PulleyMenu</name>
+    <message>
+        <source>Multiple player view</source>
+        <translation>Более двух команд</translation>
+    </message>
+    <message>
+        <source>Two teams view</source>
+        <translation>Две команды</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Помощь</translation>
+    </message>
+</context>
+<context>
+    <name>TTSetScores</name>
+    <message>
+        <source>Set scores</source>
+        <translation>Установить счет</translation>
+    </message>
+    <message>
+        <source>Home score</source>
+        <translation>Счет хозяев</translation>
+    </message>
+    <message>
+        <source>Set home score</source>
+        <translation>Установить счет хозяев</translation>
+    </message>
+    <message>
+        <source>Home score set.</source>
+        <translation>Установка счета хозяев</translation>
+    </message>
+    <message>
+        <source>Visitor score</source>
+        <translation>Счет гостей</translation>
+    </message>
+    <message>
+        <source>Set visitor score</source>
+        <translation>Установить счет гостей</translation>
+    </message>
+    <message>
+        <source>Visitor score set.</source>
+        <translation>Установка счета гостей</translation>
+    </message>
+    <message>
+        <source>Home rounds</source>
+        <translation>Раунды хозяев</translation>
+    </message>
+    <message>
+        <source>Set home rounds</source>
+        <translation>Установить раунды хозяев</translation>
+    </message>
+    <message>
+        <source>Home rounds set.</source>
+        <translation>Установка раундов хозяев</translation>
+    </message>
+    <message>
+        <source>Visitor rounds</source>
+        <translation>Гостевые раунды</translation>
+    </message>
+    <message>
+        <source>Set visitor rounds</source>
+        <translation>Установить гостевые раунды</translation>
+    </message>
+    <message>
+        <source>Visitor rounds set.</source>
+        <translation>Установка гостевых раундов</translation>
+    </message>
+    <message>
+        <source>Press enter to save</source>
+        <translation>Нажмите ввод для сохранения</translation>
+    </message>
+</context>
+<context>
+    <name>TTView</name>
+    <message>
+        <source>Resetting scores</source>
+        <translation>Переустановить счет</translation>
+    </message>
+    <message>
+        <source>Resetting rounds</source>
+        <translation>Переустановить раунды</translation>
+    </message>
+    <message>
+        <source>Saved scores</source>
+        <translation>Сохраненный счет</translation>
+    </message>
+    <message>
+        <source>Set scores &amp; rounds</source>
+        <translation>Установить счет и раунды</translation>
+    </message>
+    <message>
+        <source>Reset scores</source>
+        <translation>Сбросить счет</translation>
+    </message>
+    <message>
+        <source>Reset rounds</source>
+        <translation>Сбросить раунды</translation>
+    </message>
+    <message>
+        <source>Scoreboard</source>
+        <translation>Игровое табло</translation>
+    </message>
+    <message>
+        <source>Swap</source>
+        <translation>Поменять местами</translation>
+    </message>
+</context>
+</TS>
